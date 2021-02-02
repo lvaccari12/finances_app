f6053d79d589193c93fa5c51aa52fde69805f8c5
lvaccari12 <70210826+lvaccari12@users.noreply.github.com>
2021-02-02 22:53:53 +0000

    aplication finished

--- scripts.js
+++ scripts.js
@@ -11,29 +11,18 @@ const Modal = {
     }
 }
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
+
 const Transaction = {
-    all: [
-        {
-            description: 'Luz',
-            amount: -50000,
-            date: '21/01/2021'
-        },
-        {
-            description: 'Website',
-            amount: 500000,
-            date: '21/01/2021'
-        },
-        {
-            description: 'Internet',
-            amount: -20000,
-            date: '21/01/2021'
-        },
-        {
-            description: 'App',
-            amount: 200000,
-            date: '21/01/2021'
-        },
-    ],
+    all: Storage.get(),
 
     add(transaction){
         Transaction.all.push(transaction)
@@ -87,13 +76,14 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
 
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -103,7 +93,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
             `
 
@@ -222,15 +212,17 @@ const Form = {
     }
 }
 
+
 const App = {
     init() {
 
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance()
         
+        Storage.set(Transaction.all)
     },
     reload() {
         DOM.clearTransactions()
